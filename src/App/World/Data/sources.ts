33caa0d4c59b1ad1f6ecefb1150d18c867465f8d
theodorfransson <theofransson0203@gmail.com>
2024-03-17 16:56:52 +0100

    Added and moved around static resources

--- src/App/World/Data/sources.ts
+++ src/App/World/Data/sources.ts
@@ -16,72 +16,192 @@ const resources: Resource[] = [
     {
         name: 'model',
         type: 'gltfModel',
-        path: ['models/model.glb'],
+        path: ['models/studio/model.glb'],
     },
     {
         name: 'backdrop',
         type: 'gltfModel',
-        path: ['models/backdrop.glb'],
+        path: ['models/studio/backdrop.glb'],
     },
     {
         name: 'modelFull',
         type: 'texture',
-        path: ['textures/baked/modelFull.jpg'],
+        path: ['textures/baked/studio/modelFull.jpg'],
     },
     {
         name: 'modelBack',
         type: 'texture',
-        path: ['textures/baked/modelBack.jpg'],
+        path: ['textures/baked/studio/modelBack.jpg'],
     },
     {
         name: 'modelSide',
         type: 'texture',
-        path: ['textures/baked/modelSide.jpg'],
+        path: ['textures/baked/studio/modelSide.jpg'],
     },
     {
         name: 'modelThree',
         type: 'texture',
-        path: ['textures/baked/modelThree.jpg'],
+        path: ['textures/baked/studio/modelThree.jpg'],
     },
     {
         name: 'backdropFull',
         type: 'texture',
-        path: ['textures/baked/backdropFull.jpg'],
+        path: ['textures/baked/studio/backdropFull.jpg'],
     },
     {
         name: 'backdropBack',
         type: 'texture',
-        path: ['textures/baked/backdropBack.jpg'],
+        path: ['textures/baked/studio/backdropBack.jpg'],
     },
     {
         name: 'backdropSide',
         type: 'texture',
-        path: ['textures/baked/backdropSide.jpg'],
+        path: ['textures/baked/studio/backdropSide.jpg'],
     },
     {
         name: 'backdropThree',
         type: 'texture',
-        path: ['textures/baked/backdropThree.jpg'],
+        path: ['textures/baked/studio/backdropThree.jpg'],
     },
     {
         name: 'equipment',
         type: 'gltfModel',
-        path: ['models/equipment.glb']
+        path: ['models/weights/equipment.glb']
     }, 
     {
         name: 'ground',
         type: 'gltfModel',
-        path: ['models/ground.glb']
+        path: ['models/weights/ground.glb']
     }, 
     {
         name: 'bakedEquipment',
         type: 'texture',
-        path: ['textures/baked/Equipment.jpg']
+        path: ['textures/baked/weights/Equipment.jpg']
     },
     {
         name: 'bakedGround',
         type: 'texture',
-        path: ['textures/baked/Ground.jpg']
+        path: ['textures/baked/weights/Ground.jpg']
+    },
+    {
+        name: 'cityPart00',
+        type: 'gltfModel',
+        path: ['models/city/Part00.glb']
+    },
+    {
+        name: 'cityPart01',
+        type: 'gltfModel',
+        path: ['models/city/Part01.glb']
+    },
+    {
+        name: 'cityPart02',
+        type: 'gltfModel',
+        path: ['models/city/Part02.glb']
+    },
+    {
+        name: 'cityPart03',
+        type: 'gltfModel',
+        path: ['models/city/Part03.glb']
+    },
+    {
+        name: 'cityPart04',
+        type: 'gltfModel',
+        path: ['models/city/Part04.glb']
+    },
+    {
+        name: 'cityPart05',
+        type: 'gltfModel',
+        path: ['models/city/Part05.glb']
+    },
+    {
+        name: 'cityPart06',
+        type: 'gltfModel',
+        path: ['models/city/Part06.glb']
+    },
+    {
+        name: 'cityPart07',
+        type: 'gltfModel',
+        path: ['models/city/Part07.glb']
+    },
+    {
+        name: 'backsides',
+        type: 'gltfModel',
+        path: ['models/city/Backsides.glb']
+    },
+    {
+        name: 'screens',
+        type: 'gltfModel',
+        path: ['models/city/Screens.glb']
+    },
+    {
+        name: 'cityBaked00',
+        type: 'texture',
+        path: ['textures/baked/city/Baked00.jpg']
+    },
+    {
+        name: 'cityBaked01',
+        type: 'texture',
+        path: ['textures/baked/city/Baked01.jpg']
+    },
+    {
+        name: 'cityBaked02',
+        type: 'texture',
+        path: ['textures/baked/city/Baked02.jpg']
+    },
+    {
+        name: 'cityBaked03',
+        type: 'texture',
+        path: ['textures/baked/city/Baked03.jpg']
+    },
+    {
+        name: 'cityBaked04',
+        type: 'texture',
+        path: ['textures/baked/city/Baked04.jpg']
+    },
+    {
+        name: 'cityBaked05',
+        type: 'texture',
+        path: ['textures/baked/city/Baked05.jpg']
+    },
+    {
+        name: 'cityBaked06',
+        type: 'texture',
+        path: ['textures/baked/city/Baked06.jpg']
+    },
+    {
+        name: 'cityBakedNight00',
+        type: 'texture',
+        path: ['textures/baked/city/BakedNight00.jpg']
+    },
+    {
+        name: 'cityBakedNight01',
+        type: 'texture',
+        path: ['textures/baked/city/BakedNight01.jpg']
+    },
+    {
+        name: 'cityBakedNight02',
+        type: 'texture',
+        path: ['textures/baked/city/BakedNight02.jpg']
+    },
+    {
+        name: 'cityBakedNight03',
+        type: 'texture',
+        path: ['textures/baked/city/BakedNight03.jpg']
+    },
+    {
+        name: 'cityBakedNight04',
+        type: 'texture',
+        path: ['textures/baked/city/BakedNight04.jpg']
+    },
+    {
+        name: 'cityBakedNight05',
+        type: 'texture',
+        path: ['textures/baked/city/BakedNight05.jpg']
+    },
+    {
+        name: 'cityBakedNight06',
+        type: 'texture',
+        path: ['textures/baked/city/BakedNight06.jpg']
     }
 ]
 
